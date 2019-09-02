5fff679bf609bb39a90c69ed15eb53bdf513fb59
Daisuke Inoue <dicek8823@icloud.com>
2019-09-03 08:00:26 +0900

    creat componets

--- src/App.js
+++ src/App.js
@@ -2,22 +2,36 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">
-          bar
-        </label>
-        <input type="text" onClick={ () => {console.log("I am clicked!")}} />
-      </React.Fragment>
-    )
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
+//class App extends Component {
+//  render() {
+//    return (
+//      <React.Fragment>
+//        <label htmlFor="bar">
+//          bar
+//        </label>
+//        <input type="text" onClick={ () => {console.log("I am clicked!")}} />
+//      </React.Fragment>
+//    )
     //return <input type="text" onClick={ () => {console.log("I am clicked!")}} />;
     //const greeting = "Hi!!"
     //const dom = <h1 className="foo">{greeting}</h1>;
     //return dom;
-  }
-}
+//  }
+//}
 
 
 export default App;
